480966c1c836b0ab46ed7d66bce7d6c0705d1535
Stephen Royle <s.j.royle@warwick.ac.uk>
2020-07-19 15:17:56 +0100

    Plotting functions

--- R/cancer_figure/Script/Amplification.R
+++ R/cancer_figure/Script/Amplification.R
@@ -1,53 +1,80 @@
 library(tidyverse)
 library(RColorBrewer)
 
-df <- read.delim2("Data/alterations_across_samples_TPDs.tsv")
+tpd_filepath <- "Data/alterations_across_samples_TPDs.tsv"
+allrab_filepath <- "Data/alterations_across_samples_allRabs.tsv"
+invrab_filepath <- "Data/alterations_across_samples_INVRabs.tsv"
+ninvrab_filepath <- "Data/alterations_across_samples_NonINVRabs.tsv"
 
-summaryDF <- df %>%
-  group_by(Study.ID) %>%
-  summarise(TPD52 = sum(match(TPD52,"AMP"), na.rm = TRUE) / n(),
-            TPD52L1 = sum(match(TPD52L1,"AMP"), na.rm = TRUE) / n(),
-            TPD52L2 = sum(match(TPD52L2,"AMP"), na.rm = TRUE) / n(),
-            TPD52L3 = sum(match(TPD52L3,"AMP"), na.rm = TRUE) / n())
+make_the_df <- function(theFile)  {
+  df <- read.delim2(theFile)
+  nGenes <- (ncol(df) - 4) / 5
+  geneNames <- names(df[5:(4+nGenes)])
+  binary <- ifelse(df[,5:(4+nGenes)] == "AMP", 1, 0)
+  binary <- cbind(binary, All = rowSums(binary))
+  binary[,ncol(binary)] <- ifelse(binary[,ncol(binary)] > 0, 1, 0)
+  binary <- cbind.data.frame(Study.ID = df$Study.ID, binary)
+  tally_df <- binary %>%
+    group_by(Study.ID) %>%
+    summarise_all(sum)
+  total_df <- binary %>%
+    group_by(Study.ID) %>%
+    summarise(n = n ())
+  # calculate freq
+  tally_df[,2:(2+nGenes)] <- tally_df[,2:(2+nGenes)] / total_df$n
+  # load cancer names n.b. COAD -> COADREAD
+  # https://gdc.cancer.gov/resources-tcga-users/tcga-code-tables/tcga-study-abbreviations
+  study_lookup <- read.delim2("Data/study_lookup.txt")
+  total_df$Study.Abbreviation <- toupper(gsub("_tcga_pan_can_atlas_2018","",total_df$Study.ID))
+  total_df <- merge(total_df,study_lookup)
+  tally_df$Study.ID <- total_df$Study.Name
+  names(tally_df)[names(tally_df)=="Study.ID"] <- "Cancer"
+  # make tidy
+  df_out <- tally_df %>% 
+    mutate(Cancer = factor(Cancer)) %>%
+    gather(key = gene, value = freq, 2:(2+nGenes))
+  
+  return(df_out)
+}
 
-# add all frequencies from gene columns
-summaryDF$All <- rowSums(summaryDF[,c(2,3,4,5)])
+make_the_plot <- function(df_for_plotting){
+  thePlot <- ggplot(data = df_for_plotting, aes(x = gene, y = Cancer, fill = freq)) +
+    geom_tile(colour="white",size=0.25) +
+    guides(fill = guide_legend(title = "Frequency", reverse = TRUE)) +
+    ylim(rev(levels(tpd_df$Cancer))) +
+    coord_equal() +
+    scale_fill_gradient(low = "#fee0d2", high = "#de2d26") +
+    labs(x = NULL, y = NULL) +
+    theme_minimal(base_size=10) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  
+  return(thePlot)
+}
 
-# load cancer names
-# https://gdc.cancer.gov/resources-tcga-users/tcga-code-tables/tcga-study-abbreviations
-study_lookup <- read.delim2("Data/study_lookup.txt")
-summaryDF$Study.Abbreviation <- toupper(gsub("_tcga_pan_can_atlas_2018","",summaryDF$Study.ID))
-summaryDF <- merge(summaryDF,study_lookup)
-# make df more simple and change names
-keeps <- c("Study.Name", "All", "TPD52", "TPD52L1", "TPD52L2", "TPD52L3")
-summaryDF <- summaryDF[keeps]
-names(summaryDF)[names(summaryDF)=="Study.Name"] <- "Cancer"
-# make tidy
-summary_df_long <- summaryDF %>% 
-  gather(key = gene, value = freq, 2:6)
+# TPD plots
+tpd_df <- make_the_df(tpd_filepath)
+p1 <- make_the_plot(subset(tpd_df, gene != "All"))
+p2 <- make_the_plot(subset(tpd_df, gene == "All"))
+ggsave("Output/Plots/TPDs.pdf", plot = p1)
+ggsave("Output/Plots/TPDsCombined.pdf", plot = p2)
 
-p1 <- ggplot(data = summary_df_long, aes(x = Cancer, y = gene, fill = freq)) +
-  geom_tile() +
-  coord_flip() +
-  scale_fill_gradient(low = "#fee0d2", high = "#de2d26") +
-  labs(x = "", y = "") +
-  theme_minimal()
+# All Rabs plots
+allRabs_df <- make_the_df(allrab_filepath)
+p3 <- make_the_plot(subset(allRabs_df, gene != "All"))
+p4 <- make_the_plot(subset(allRabs_df, gene == "All"))
+ggsave("Output/Plots/allRabs.pdf", plot = p3)
+ggsave("Output/Plots/allRabsCombined.pdf", plot = p4)
 
-ggsave("Output/Plots/TPD.pdf", plot = p1, useDingbats = FALSE)
+# INV Rabs plots
+invRabs_df <- make_the_df(invrab_filepath)
+p5 <- make_the_plot(subset(invRabs_df, gene != "All"))
+p6 <- make_the_plot(subset(invRabs_df, gene == "All"))
+ggsave("Output/Plots/invRabs.pdf", plot = p5)
+ggsave("Output/Plots/invRabsCombined.pdf", plot = p6)
 
-rab_all <- read.delim2("Data/alterations_across_samples_allRabs.tsv")
-
-summary_rab_all <- rab_all %>%
-  group_by(Study.ID) %>%
-  summarise(all = sum(Altered) / n())
-
-summary_rab_all$Study.Abbreviation <- toupper(gsub("_tcga_pan_can_atlas_2018","",summary_rab_all$Study.ID))
-summary_rab_all <- merge(summary_rab_all,study_lookup)
-names(summary_rab_all)[names(summary_rab_all)=="Study.Name"] <- "Cancer"
-
-p2 <- ggplot(data = summary_df_long, aes(x = Cancer, y = all, fill = all)) +
-  geom_tile() +
-  coord_flip() +
-  scale_fill_gradient(low = "#fee0d2", high = "#de2d26") +
-  labs(x = "", y = "") +
-  theme_minimal()
+# non-INV Rabs plots
+ninvRabs_df <- make_the_df(ninvrab_filepath)
+p7 <- make_the_plot(subset(ninvRabs_df, gene != "All"))
+p8 <- make_the_plot(subset(ninvRabs_df, gene == "All"))
+ggsave("Output/Plots/ninvRabs.pdf", plot = p7)
+ggsave("Output/Plots/ninvRabsCombined.pdf", plot = p8)
